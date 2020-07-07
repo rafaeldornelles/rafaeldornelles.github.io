81d229f9d8222b5fabe67e5071176a99cefe3deb
Rafael <rafafd@hotmail.com>
2020-07-07 16:47:30 -0300

    removing links from login screen

--- js/app.js
+++ js/app.js
@@ -1,19 +1,9 @@
-// const vaccineCardController =  new VaccineController();
-// const patientController = new PatientController();
-const pacienteController =  new PacienteController();
-const dashboardController = new DashboardController()
-const loginController = new LoginController(pacienteController);
-const navbarController = new NavbarController()
+const produtoController =  new ProdutoController();
 
 var body = document.querySelector("body");
 body.onload = function () {
-    navbarController.loadNavbar()
-    // patientController.loadPatients();
-    if (localStorage.getItem("token")){
-        dashboardController.loadDashboard();
-        navbarController.makeLinkActive(0)
-    } else {
-        loginController.loginForm();
-    }
-    // vaccineCardController.loadVaccines();
+    //produtoController.carregarFormulario();
+    produtoController.inicializa();
 }
+
+
