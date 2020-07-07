c2671fa88a2dedd17833103ac6a0e2e392f71dfe
Rafael <rafafd@hotmail.com>
2020-07-07 17:10:21 -0300

    fixin

--- js/app.js
+++ js/app.js
@@ -7,16 +7,14 @@ const navbarController = new NavbarController()
 
 var body = document.querySelector("body");
 body.onload = function () {
-    navbarController.loadNavbar()
     // patientController.loadPatients();
     if (localStorage.getItem("token")){
+        navbarController.loadNavbar()
         dashboardController.loadDashboard();
         navbarController.makeLinkActive(0)
     } else {
+        navbarController.loadWithoutLinks()
         loginController.loginForm();
     }
     // vaccineCardController.loadVaccines();
-
 }
-
-
